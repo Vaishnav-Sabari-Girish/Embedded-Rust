e587a2fcf66f08e74da5edbba1e4adcc09ef8ef3
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-14 13:59:47 +0530

    DId some stuff

--- microcontrollers/esp32/hello_world/src/bin/main.rs
+++ microcontrollers/esp32/hello_world/src/bin/main.rs
@@ -9,12 +9,9 @@ esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.4.0
     let delay = Delay::new();
-
     loop {
         println!("Hello World");
         delay.delay_millis(500);
     }
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.1/examples/src/bin
 }
