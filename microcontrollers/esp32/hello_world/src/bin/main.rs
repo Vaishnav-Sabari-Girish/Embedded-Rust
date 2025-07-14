ed77240a637883e46c961c048b89591ec3c80612
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-14 14:15:05 +0530

    Made changes to hello World

--- microcontrollers/esp32/hello_world/src/bin/main.rs
+++ microcontrollers/esp32/hello_world/src/bin/main.rs
@@ -2,13 +2,19 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, main};
+use esp_hal::{
+    clock::CpuClock,
+    delay::Delay, 
+    main
+};
 use esp_println::println;
 esp_bootloader_esp_idf::esp_app_desc!();
 
 
 #[main]
 fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let _peripherals = esp_hal::init(config);
     let delay = Delay::new();
     loop {
         println!("Hello World");
