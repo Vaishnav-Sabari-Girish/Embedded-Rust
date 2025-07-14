e338bec4723f95aacfb7e6685518aa1154efb16f
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-14 10:25:06 +0530

    Added ultrasonic sensor esp32

--- microcontrollers/esp32/sensor_reading/sensor_reading.md
+++ microcontrollers/esp32/sensor_reading/sensor_reading.md
@@ -1,3 +1,4 @@
 # Sensor Readings in Embedded Rust
 
 1. [DHT22](./dht22_sensor_reading/src/bin/main.rs)
+2. [Ultrasonic Sensor](./ultrasonic_sensor/src/bin/main.rs)
---
+++ microcontrollers/esp32/sensor_reading/ultrasonic_sensor/src/bin/main.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::CpuClock, 
+    delay::Delay, 
+    gpio::{Input, InputConfig, Level, Output, OutputConfig}, 
+    main, 
+};
+use esp_println::println;
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripehrals = esp_hal::init(config);
+
+    let echo_pin = Input::new(peripehrals.GPIO4, InputConfig::default());
+    let mut trig_pin = Output::new(peripehrals.GPIO2, Level::Low, OutputConfig::default());
+    
+    let delay = Delay::new();
+
+    loop {
+        trig_pin.set_low();
+        delay.delay_micros(2);
+        trig_pin.set_high();
+        delay.delay_micros(10);
+        trig_pin.set_low();
+
+        //Wait for echo pin to go high
+        while echo_pin.is_low() {}
+
+        //Measure pulse duration
+        let start = esp_hal::time::Instant::now();
+        while echo_pin.is_high() {}
+        let end = esp_hal::time::Instant::now();
+
+        let pulse_duration = start.elapsed() - end.elapsed();
+
+        let distance_cm = (pulse_duration.as_micros() as f32 * 0.0343) / 2.0;
+
+        println!("Distance: {}", distance_cm);
+        delay.delay_millis(100);
+    }
+}
