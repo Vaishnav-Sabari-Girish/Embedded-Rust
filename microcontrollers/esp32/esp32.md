d1f4b2591009b2cfe14ceaed3c8eff1bf0fa2f8a
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-17 21:29:23 +0530

    Added PWM program

--- microcontrollers/esp32/esp32.md
+++ microcontrollers/esp32/esp32.md
@@ -5,3 +5,4 @@
 3. [LED Blink type 2](./led_blink_type_2/src/bin/main.rs)
 4. [Button press](./button_press/src/bin/main.rs)
 5. [Sensor Interfacing](./sensor_reading/sensor_reading.md)
+6. [PWM](./led_pwm/src/bin/main.rs)
---
+++ microcontrollers/esp32/led_pwm/src/bin/main.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::{
+    clock::CpuClock,
+    ledc::{
+        channel,
+        channel::ChannelIFace,
+        timer::TimerIFace,
+        timer,
+        LSGlobalClkSource,
+        Ledc,
+        LowSpeed
+    },
+    time::Rate,
+    main
+};
+
+use log::info;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let led = peripherals.GPIO2;
+
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut lstimer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+
+    lstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: Rate::from_khz(24),
+        })
+    .unwrap();
+
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
+
+    channel0
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+    .unwrap();
+
+    loop {
+        channel0.start_duty_fade(0, 100, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
+        channel0.start_duty_fade(100, 0, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
+    }
+
+}
